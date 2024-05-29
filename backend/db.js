9f15920fb3a5ce998fdbb48956242d9b61018f26
unknown <ankitgarg1102@gmail.com>
2024-05-29 16:52:05 +0530

    commits

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+//const mongoose = require('mongoose');
+//require("dotenv").config();
+// database connection with mongodb
+//module.exports.init = async function(){
+  //  await mongoose.connect("mongodb+srv://ankitreactdev:8888809887@cluster0.3yquoxm.mongodb.net/e-commerce");
+    //}
+
